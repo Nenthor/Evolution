1cbd7fc2f9859ac201a2a62aaa34a88e6f784173
TheDavido <nentwich05@gmail.com>
2022-12-04 12:10:40 +0100

    Added sensor filter

--- Server/src/backend/hardware/sensors.py
+++ Server/src/backend/hardware/sensors.py
@@ -1,5 +1,5 @@
-import time as __time
-import threading as __threading
+from time import sleep as __sleep, time as __time
+from threading import Thread as __Thread, Lock as __Lock
 import gpio as __gpio
 
 SENSOR_1 = 1
@@ -15,21 +15,20 @@ __GPIO_TRIGGER_3 = None
 __GPIO_ECHO_3 = None
 
 __isActive = False
-__lock = __threading.Lock()
-__timer1 = None
-__timer2 = None
-__timer3 = None
+__lock = __Lock()
+__timer = [None, None, None]
+__data: list[tuple[float, int]]
 
 
 def __startLoop():
     try:
         while __isActive:
             __sendSignal(__GPIO_TRIGGER_1)
-            __time.sleep(0.010)
+            __sleep(0.010)
             __sendSignal(__GPIO_TRIGGER_2)
-            __time.sleep(0.010)
+            __sleep(0.010)
             __sendSignal(__GPIO_TRIGGER_3)
-            __time.sleep(0.060)
+            __sleep(0.060)
     except Exception:
         stop()
 
@@ -37,49 +36,59 @@ def __startLoop():
 def __sendSignal(channel):
     if channel != None:
         __gpio.output(channel, __gpio.HIGH)
-        __time.sleep(0.000010)  # 10 microseconds
+        __sleep(0.000010)  # 10 microseconds
         __gpio.output(channel, __gpio.LOW)
 
 
 def __onEdgeEvent(channel):
+    global __timer
     if channel == __GPIO_ECHO_1:
-        global __timer1
-        if __timer1 == None:
-            __timer1 = __time.time()  # Rising event
+        if __timer[0] == None:
+            __timer[0] = __time()  # Rising event
         else:
-            elapsedTime = __time.time() - __timer1
+            elapsedTime = __time() - __timer[0]
             distance = int((elapsedTime * 34300) / 2)
-            __timer1 = None
+            __timer[0] = None
             if distance <= 450:
-                onData(SENSOR_1, distance)
+                __onNewData(SENSOR_1, distance)
             else:
-                onData(SENSOR_1, 500)
+                __onNewData(SENSOR_1, 500)
     elif channel == __GPIO_ECHO_2:
-        global __timer2
-        if __timer2 == None:
-            __timer2 = __time.time()  # Rising event
+        if __timer[1] == None:
+            __timer[1] = __time()  # Rising event
         else:
-            elapsedTime = __time.time() - __timer2
+            elapsedTime = __time() - __timer[1]
             distance = int((elapsedTime * 34300) / 2)
-            __timer2 = None
+            __timer[1] = None
             if distance <= 450:
-                onData(SENSOR_2, distance)
+                __onNewData(SENSOR_2, distance)
             else:
-                onData(SENSOR_2, 500)
+                __onNewData(SENSOR_2, 500)
     elif channel == __GPIO_ECHO_3:
-        global __timer3
-        if __timer3 == None:
-            __timer3 = __time.time()  # Rising event
+        if __timer[2] == None:
+            __timer[2] = __time()  # Rising event
         else:
-            elapsedTime = __time.time() - __timer3
+            elapsedTime = __time() - __timer[2]
             distance = int((elapsedTime * 34300) / 2)
-            __timer3 = None
+            __timer[2] = None
             if distance <= 450:
-                onData(SENSOR_3, distance)
+                __onNewData(SENSOR_3, distance)
             else:
-                onData(SENSOR_3, 500)
+                __onNewData(SENSOR_3, 500)
     else:
         print("channel is not defined.")
+
+
+def __onNewData(sensor, distance):
+    global __data, __lock
+    (oldTime, oldDistance) = __data[sensor - 1]
+    time = __time()
+
+    if time - oldTime >= 1.0 or distance < oldDistance:
+        # Filter: Set data if longer than 1s or distance is smaller
+        with __lock:
+            __data[sensor - 1] = (time, distance)
+        onData(sensor, distance)
 
 
 def onData(sensor, distance):
@@ -92,14 +101,14 @@ def stop():
     global __lock, __isActive
     with __lock:
         __isActive = False
-    __time.sleep(0.1)
+    __sleep(0.1)
     __gpio.setwarnings(False)
     __gpio.cleanup()
 
 
 def start():
     """Activate sensors."""
-    global __lock, __isActive
+    global __lock, __isActive, __data
     with __lock:
         __isActive = True
     __gpio.setmode(__gpio.BCM)
@@ -118,5 +127,7 @@ def start():
         __gpio.setup(__GPIO_ECHO_3, __gpio.IN)
         __gpio.add_event_detect(__GPIO_ECHO_3, __gpio.BOTH, callback=__onEdgeEvent)
 
-    thread = __threading.Thread(target=__startLoop, daemon=True)
+    __data = [(__time(), 500), (__time(), 500), (__time(), 500)]
+
+    thread = __Thread(target=__startLoop, daemon=True)
     thread.start()
